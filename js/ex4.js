5b9824951c965546c3acc48ddfba53cc7ef7fd5d
andrademiguel <miguel4andrade@gmail.com>
2020-10-13 14:53:08 -0700

    complete

--- js/ex4.js
+++ js/ex4.js
@@ -1,10 +1,19 @@
-// // const link = [
-// //     "https://www.csulb.edu/college-of-business"
-// // ];
+const links = [
+    "https://www.csulb.edu/college-of-business"
+];
 
-// // const contentElement = document.getElementsByTagName
+const contentElement = document.getElementsByTagName("ul")[2];
+links.forEach(link => {
+    const linkElement = document.createElement("a");
+    linkElement.textContent = "College Of Business";
+    linkElement.href = link;
+    
+    contentElement.appendChild(document.createElement("li"));
+    contentElement.appendChild(linkElement);
+    
+});
 
 // document.getElementsByTagName("li").innerHTM: += '<li><a href=https://www.csulb.edu/college-of-business'>College of Business</li>';
 
-var newListItem = document.createElement('li');
-newListItem.textContent = "College of Business";
+// var newListItem = document.createElement('li');
+// newListItem.textContent = "College of Business";
